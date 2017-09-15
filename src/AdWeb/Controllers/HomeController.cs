82630192f779753b17724d905565f2ce31d9c927
Simon Duff [Contractor] <simon.duff@just-eat.com>
2017-09-15 13:25:53 +0100

    Initial commit

---
+++ src/AdWeb/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AdWeb.Models;
+using Novell.Directory.Ldap;
+
+namespace AdWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        [HttpPost]
+        public IActionResult Login(AuthModel model)
+        {
+            LdapConnection connection = null;
+            try
+            {
+                connection = new LdapConnection();
+                connection.Connect("host", 80); // Change these
+                connection.Bind(model.Username, model.Password);
+                model.Message = "Succeeded";
+            }
+            catch (Exception e)
+            {
+                model.Message = $"Failed: {e.ToString()}";
+            }
+            finally
+            {
+                connection?.Disconnect();
+                connection?.Dispose();
+            }
+
+            return View("Index", model);
+        }
+    }
+}
---
+++ src/AdWeb/Models/AuthModel.cs
@@ -0,0 +1,9 @@
+﻿namespace AdWeb.Models
+{
+    public class AuthModel
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Message { get; set; }
+    }
+}
